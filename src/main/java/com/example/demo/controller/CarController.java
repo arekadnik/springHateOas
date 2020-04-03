8cf4881ee7e53232eeae4efb397fdf15f33a6846
areka <arekadnik@gmail.com>
2020-04-03 12:34:53 +0200

    hateOAS

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.model.Car;
 import com.example.demo.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,23 +29,30 @@ public class CarController {
     }
 
     @GetMapping
-    public ResponseEntity<java.util.List<Car>> getCars() {
+    public ResponseEntity<CollectionModel<Car>> getCars() {
         List<Car> allCars = carService.getAllCars();
-        return new ResponseEntity(allCars, HttpStatus.OK);
+        allCars.forEach(car -> car.add(linkTo(CarController.class).slash(car.getCarID()).withSelfRel()));
+        Link link = linkTo(CarController.class).withSelfRel();
+        CollectionModel<Car> collectionModel = new CollectionModel(allCars, link);
+        return new ResponseEntity(collectionModel, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EntityModel<Car>> getCarByID(@PathVariable long id) {
         org.springframework.hateoas.Link link = linkTo(CarController.class).slash(id).withSelfRel();
         Optional<Car> carById = carService.getCarById(id);
-        EntityModel<Car> entityModel = new EntityModel(carById,link);
+        EntityModel<Car> entityModel = new EntityModel(carById, link);
         return new ResponseEntity<>(entityModel, HttpStatus.OK);
     }
 
     @GetMapping("/color/{color}")
-    public ResponseEntity<List<Car>> getCarByColor(@PathVariable String color) {
+    public ResponseEntity<CollectionModel<Car>> getCarByColor(@PathVariable String color) {
         List<Car> carList = carService.getCarsByColor(color);
-        return new ResponseEntity(carList, HttpStatus.OK);
+        carList.forEach(car -> car.add(linkTo(CarController.class).slash(car.getCarID()).withSelfRel()));
+        carList.forEach(car -> car.add(linkTo(CarController.class).withRel("allColors")));
+        Link link = linkTo(CarController.class).withSelfRel();
+        CollectionModel<Car> collectionModel = new CollectionModel(carList, link);
+        return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
 }
